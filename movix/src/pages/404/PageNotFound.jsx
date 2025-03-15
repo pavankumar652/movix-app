e8df1fbdb2e3afc11d549b3f15b9bacc7b95b6ee
pavankumar <pavankumarponduru652@gmail.com>
2025-03-15 23:00:17 +0530

    9th commit

---
+++ movix/src/pages/404/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.scss";
+
+const PageNotFound = () => {
+    return (
+        <div className="page-not-found">
+            <div className="error-message">Page Not Found</div>
+        </div>
+    );
+};
+
+export default PageNotFound;
